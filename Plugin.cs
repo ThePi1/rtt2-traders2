63a5b1d8c6b7c23c855516c760cc7ba812fe69c1
house <spthouse16@gmail.com>
2026-05-18 21:16:26 -0500

    MWQC reapplied

--- Plugin.cs
+++ Plugin.cs
@@ -103,7 +103,7 @@ public class rtt2trader(
         var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
 
         var VanillaItemsList = modHelper.GetJsonDataFromFile<List<string>>(pathToMod, "data/itemTPL.json");
-        //var weapQuestList = modHelper.GetJsonDataFromFile<Dictionary<string, List<string>>>(pathToMod,"data/WeaponQuestCatagories.json");
+        var weapQuestList = modHelper.GetJsonDataFromFile<Dictionary<string, List<string>>>(pathToMod,"data/WeaponQuestCatagories.json");
 
         
         VanillaItems = new HashSet<string>(VanillaItemsList); //use a hashset instead of list to save on runtime when we compare items.
@@ -225,7 +225,7 @@ public class rtt2trader(
         logger.LogWithColor("RTT2 has completed all steps!",LogTextColor.Blue,LogBackgroundColor.Black);
 
         // Send back a success to the server to say our trader is good to go
-        //addModdedWeaponToQuests(weapQuestList);
+        addModdedWeaponToQuests(weapQuestList);
         clock.Stop();
         logger.Info("Completed in " + clock.ElapsedMilliseconds + "ms");
         await Task.CompletedTask;
@@ -477,7 +477,7 @@ public class rtt2trader(
             }
         }
     }
-    /*private void addModdedWeaponToQuests(Dictionary<string, List<string>>weapQuest)
+    private void addModdedWeaponToQuests(Dictionary<string, List<string>>weapQuest)
     {
         var items = databaseService.GetTables().Templates.Items;
         foreach (var (catagory, qIDs) in weapQuest) //grab parent id and list of quests to change
@@ -488,22 +488,23 @@ public class rtt2trader(
                 break;
             }
                 
-            foreach (var questId in qIDs)
+            foreach (var questId in qIDs) //run through each quest listed in in WeaponQuestCat.json
             {
-                foreach (var weap in moddedWeap){
+                foreach (var weap in moddedWeap){ //run through list of modded weapons that meet the quest catagory
                     var quests = databaseService.GetTables().Templates.Quests;
-                    if(!quests.TryGetValue(questId,out var quest))
+                    if(!quests.TryGetValue(questId,out var quest)) //if quest doesnt exist continue
                     {
+                        logger.Warning("Quest: " + quest.Name + " not found\n");
                         continue;
                     }
 
-                    foreach (var condition in quest.Conditions.AvailableForFinish)
+                    foreach (var condition in quest.Conditions.AvailableForFinish) //for each counter condition we check if it has weapon key
                     {
                         foreach (var counter in condition.Counter.Conditions)
                         {
-                            if (!counter.Weapon.Contains(weap.Id))
+                            if (!counter.Weapon.Contains(weap.Id)) //for each counter condition we check if it has our modded weapon
                             {
-                                counter.Weapon.Add(weap.Id);
+                                counter.Weapon.Add(weap.Id); //add weapon id to quest weapon[] Key
                                 logger.Info("Added: " + weap.Id + " to " + questId);
                             }
                         }
@@ -511,7 +512,7 @@ public class rtt2trader(
                 }
             }
         }
-    }*/
+    }
     private void moddedItemGrabber(string trader)
     {
         var assort = databaseService.GetTrader("5ac3b934156ae10c4430e83c").Assort;
